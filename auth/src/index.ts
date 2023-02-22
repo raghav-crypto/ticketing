98253d93482a3d913d48600ac73fb3d2db74d66c
raghav-crypto <raghavjgarwal@gmail.com>
2023-02-22 17:08:33 +0530

    k8s mongo service | mongoose conn

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import 'express-async-errors';
-
+import mongoose from 'mongoose';
 import { currentUserRouter } from './routes/current-user';
 import { signinRouter } from './routes/signin';
 import { signoutRouter } from './routes/signout';
@@ -21,7 +21,19 @@ app.all('*', async (_, __) => {
 });
 
 app.use(errorHandler);
+const start = async () => {
+    try {
+        mongoose.set("strictQuery", false);
+        await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {
 
-app.listen(3000, () => {
-    console.log('Listening on port 3000!!!');
-});
+        });
+        console.log('Connected to MongoDb');
+    } catch (err) {
+        console.error(err);
+    }
+
+    app.listen(3000, () => {
+        console.log('Listening on port 3000!');
+    });
+};
+start()
